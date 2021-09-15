551ed23738c4cf2f4f1504226a7e53b04248ee8f
Luis Martinez <malbertoa_11@hotmail.com>
2021-09-15 00:13:35 -0400

    Add base table & instance

--- lib/boardroom-stack-stack.ts
+++ lib/boardroom-stack-stack.ts
@@ -1,9 +1,37 @@
 import * as cdk from '@aws-cdk/core';
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class BoardroomStackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const subscriptionsTable = new Table(this, 'DynamoDBSubscriptionsTable', {
+      tableName: 'subscriptions',
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    const vpc = new ec2.Vpc(this, 'VPC');
+
+    const defaultSecurityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Allow ssh access to ec2 instances',
+      allowAllOutbound: true,
+    });
+    defaultSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access from the world');
+
+    const instance = new ec2.Instance(this, 'BotInstance', {
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: ec2.MachineImage.genericLinux({
+        'us-east-1': 'ami-09e67e426f25ce0d7',
+      }),
+      securityGroup: defaultSecurityGroup,
+    });
+
   }
 }
